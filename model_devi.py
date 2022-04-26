21e2399aa0ff0da6e77b05a14d6ea2a49f1bd227
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-04-27 00:08:41 +0800

    auto

--- model_devi.py
+++ model_devi.py
@@ -70,17 +70,15 @@ def def_model_devi_atom(
     plt.savefig(str_save, bbox_inches='tight')
     plt.show()
 
-def def_dump2pdb(
+def def_dump2ase(
         array_id,
         type_map,       # ["O", "H"]
-        str_save = "traj.pdb"
         ):
     dp_sys = dpdata.System()
     for int_i in array_id:
-        str_origin = "traj/{}.lammpstrj".format(int_i)
+        str_origin = f"traj/{int_i}.lammpstrj"
         dp_tmp = dpdata.System(str_origin, fmt='lammps/dump', type_map=type_map)
 
         dp_sys.append(dp_tmp)
     print(dp_sys)
-    ase_atoms = dp_sys.to('ase/structure')
-    ase.io.write(format="proteindatabank", filename=str_save, images=ase_atoms)
+    return dp_sys.to('ase/structure')
--- validation.py
+++ validation.py
@@ -1,18 +1,10 @@
 import dpdata
 import numpy as np
-from string import Template
 from dpgen.generator.lib.utils import create_path
 import os
 import ase.io
 from dpgen.remote.decide_machine import convert_mdata
 import json
-from dpgen.dispatcher.Dispatcher import make_submission
-import glob
-
-fp_name = '02.fp'
-
-def def_id2dir(int_id):
-    return f"task.{int_id:06d}"
 
 def make_fp_pwscf():
     array_id = np.arange(
@@ -68,72 +60,6 @@ def make_fp_pwscf():
             pseudopotentials = dict_pwpseudop )
         
         os.chdir(str_dir_cwd)
-def make_fp (
-             jdata,
-             mdata) :
-    fp_style = jdata['fp_style']
-
-    if fp_style == "pwscf" :
-        make_fp_pwscf(jdata)
-    else :
-        raise RuntimeError ("unsupported fp style")
-    # Copy user defined forward_files
-    work_path = fp_name
-    #symlink_user_forward_files(mdata=mdata, task_type="fp", work_path=work_path)
-
-def run_fp_inner (
-                  jdata,
-                  mdata,
-                  forward_files,
-                  backward_files,
-                  log_file = "fp.log",
-                  forward_common_files=[]) :
-    fp_command = mdata['fp_command']
-    fp_group_size = mdata['fp_group_size']
-    fp_resources = mdata['fp_resources']
-
-    work_path = fp_name
-
-    fp_tasks = glob.glob(os.path.join(work_path, 'task.*'))
-    fp_tasks.sort()
-    if len(fp_tasks) == 0 :
-        return
-
-    fp_run_tasks = fp_tasks
-    run_tasks = [os.path.basename(ii) for ii in fp_run_tasks]
-
-    user_forward_files = mdata.get("fp" + "_user_forward_files", [])
-    forward_files += [os.path.basename(file) for file in user_forward_files]
-    backward_files += mdata.get("fp" + "_user_backward_files", [])
-
-    submission = make_submission(
-        mdata['fp_machine'],
-        mdata['fp_resources'],
-        commands=[fp_command],
-        work_path=work_path,
-        run_tasks=run_tasks,
-        group_size=fp_group_size,
-        forward_common_files=forward_common_files,
-        forward_files=forward_files,
-        backward_files=backward_files,
-        outlog = log_file,
-        errlog = log_file)
-    submission.run_submission()
-
-def run_fp (
-            jdata,
-            mdata) :
-    '''
-    https://github.com/deepmodeling/dpgen/blob/dc57c9a2e7ada3cf1c48c6b8bc9d475bb95ebf40/dpgen/generator/run.py#L2426-L2470
-    '''
-    fp_style = jdata['fp_style']
-    fp_pp_files = jdata['fp_pp_files']
-    if fp_style == "pwscf" :
-        forward_files = ['input'] #+ fp_pp_files
-        backward_files = ['output']
-        run_fp_inner(jdata, mdata,  forward_files, backward_files, log_file = 'output')
-    else :
-        raise RuntimeError ("unsupported fp style")
 
 def read_para_machine(
         param_file = 'param.json', 
