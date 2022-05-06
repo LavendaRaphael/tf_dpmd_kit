1c8566063f9c7dc27cfd25b019e8ca20a218ae75
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-05-06 20:52:56 +0800

    auto

--- train.py
+++ train.py
@@ -2,14 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def def_plt_lcurve():
+    fig, ax = plt.subplots()
+
     data = np.genfromtxt("lcurve.out", names=True)
-    for name in data.dtype.names[1:-1]:
-        plt.plot(data['step'], data[name], label=name)
-    plt.legend()
-    plt.xlabel('Step')
-    plt.ylabel('Loss')
-    plt.xscale('symlog')
-    plt.yscale('symlog')
-    plt.grid()
+    for name in data.dtype.names[2:-2]:
+        ax.plot(data['step'], data[name], label=name)
+    ax.legend()
+    ax.set_xlabel('Step')
+    ax.set_ylabel('Loss')
+    ax.set_xscale('symlog')
+    ax.set_yscale('log')
+    ax.set_xlim((1,1e6))
+    ax.grid()
     plt.savefig('lcurve.pdf', bbox_inches='tight')
     plt.show()
