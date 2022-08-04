8230b3baf4c665ea1c784affcde7b4eac893a0b9
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-08-04 20:48:05 +0800

    auto

--- do/do.plm.cv_plt.py
+++ do/do.plm.cv_plt.py
@@ -2,33 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
-'''
-data = np.genfromtxt("COLVAR", dtype=None, names=['time','cv1','cv2','cv3'], usecols=(0,1,2,3))
-data['time'] /= 2.0
-ax.plot(data['time'], data['cv1'], label='cn(O_126-H)')
-ax.plot(data['time'], data['cv2'], label='cn(O_127-H)')
-ax.plot(data['time'], data['cv3'], label='cn(O_128-H)')
-str_save = 'cv_cn.pdf'
-#'''
-'''
-data = np.genfromtxt("COLVAR", dtype=None, names=['time','cv1','cv2','cv3'], usecols=(0,4,5,6))
-data['time'] /= 2.0
-ax.plot(data['time'], data['cv1'], label='T(O_126-H)')
-ax.plot(data['time'], data['cv2'], label='T(O_127-H)')
-ax.plot(data['time'], data['cv3'], label='T(O_128-H)')
-str_save = 'cv_t.pdf'
-#'''
-#'''
-data = np.genfromtxt("COLVAR", dtype=None, names=['time','cv1','cv2','cv3'], usecols=(0,7,8,9))
-data['time'] /= 2.0
-ax.plot(data['time'], data['cv1'], label='T(O_126-V)')
-ax.plot(data['time'], data['cv2'], label='T(O_127-V)')
-ax.plot(data['time'], data['cv3'], label='T(O_128-V)')
-str_save = 'cv_t_com.pdf'
-#'''
+
+with open('COLVAR', 'r') as colvar:
+    list_header = colvar.readline().split()[2:]
+data = np.genfromtxt("COLVAR", dtype=None, names=list_header)
+print(data.dtype)
+#data['time'] /= 2.0
+
+list_label = [
+    #"dist_pro_c",
+    #'cn_o_126_h',
+    #'cn_o_127_h',
+    #'cn_o_128_h',
+    #'del_cn_o_h',
+    #'cn_c_pro',
+    #'cn_c_pro_1',
+    #'cost_o_126_h',
+    #'cost_o_127_pro',
+    #'cost_o_128_pro',
+    #'cost_o_127_vh',
+    #'cost_o_128_vh',
+    #'cost_o_127_newp',
+    #'cost_o_128_newp',
+    #'restraintbias',
+    #'uwallbias',
+    #'uwallforce2',
+    'dist_pro_o_127',
+    #'dist_pro_o_128',
+]
+
+for str_label in list_label:
+    ax.plot(data['time'], data[str_label], label=str_label)
+#str_save = 'cv_t_com.pdf'
 
 ax.legend()
 ax.set_xlabel('Time(ps)')
 ax.set_ylabel('CV')
-plt.savefig(str_save, bbox_inches='tight')
+#plt.savefig(str_save, bbox_inches='tight')
 plt.show()
