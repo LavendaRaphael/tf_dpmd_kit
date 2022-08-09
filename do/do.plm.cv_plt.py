90d6a1a6ef84e768f8ed61ae76effede2503a418
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-08-09 20:40:49 +0800

    auto

--- do/do.plm.cv_plt.py
+++ do/do.plm.cv_plt.py
@@ -1,44 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
+def def_plt(
+    dict_label: list,
+    str_save: str = None
+) -> None:
+    """
 
-with open('COLVAR', 'r') as colvar:
-    list_header = colvar.readline().split()[2:]
-data = np.genfromtxt("COLVAR", dtype=None, names=list_header)
-print(data.dtype)
-#data['time'] /= 2.0
+    """
+    fig, ax = plt.subplots()
+    
+    with open('COLVAR', 'r') as colvar:
+        list_header = colvar.readline().split()[2:]
+    data = np.genfromtxt("COLVAR", dtype=None, names=list_header)
+    #print(data.dtype)
+    
+    for str_label in dict_label:
+        ax.plot(data['time'], data[str_label], label=str_label)
+    
+    ax.legend()
+    ax.set_xlabel('Time(ps)')
+    ax.set_ylabel('CV')
+    if str_save:
+        plt.savefig(str_save, bbox_inches='tight')
+    plt.show()
 
-list_label = [
-    'cn_o_126_h',
-    'cn_o_127_h',
-    'cn_o_128_h',
-    #'del_cn_o_h',
-    #"dist_pro_c",
-    #'dist_pro_o_127',
-    #'dist_pro_o_128',
-    #'cn_c_pro',
-    #'cn_c_pro_1',
-    #'cost_o_126_h',
-    #'cost_o_127_pro',
-    #'cost_o_128_pro',
-    #'cost_o_127_vh',
-    #'cost_o_128_vh',
-    #'cost_o_127_newp',
-    #'cost_o_128_newp',
-    #'restraintbias',
-    #'uwallbias',
-    #'uwallforce2',
-]
-str_save = 'cv_cn.pdf'
-#str_save = 'cv_dist.pdf'
-#str_save = 'cv_cost.pdf'
-
-for str_label in list_label:
-    ax.plot(data['time'], data[str_label], label=str_label)
-
-ax.legend()
-ax.set_xlabel('Time(ps)')
-ax.set_ylabel('CV')
-plt.savefig(str_save, bbox_inches='tight')
-plt.show()
+def_plt(
+    dict_label = {
+        #"dist_pro_c",
+        #"cn_o_1_h",
+        #'cn_o_2_h',
+        #'del_cn_o_h',
+        #'cn_c_pro',
+        #'cn_c_pro_x',
+        #'cost_o_1_pro', 
+        #'cost_o_2_pro',
+        #'cost_o_1_vh', 
+        #'cost_o_2_vh', 
+        #'cost_o_1_h', 
+        #'cost_o_2_h', 
+        #'restraint.bias',
+        #'up_c_pro.bias',
+        #'up_c_pro.force2', 
+        #'dist_o_0_h',
+        #'up_o_0_h.bias', 
+        #'up_o_0_h.force2', 
+        #'cost_o_0_h', 
+        #'dist_pro_o_1', 
+        #'dist_pro_o_2', 
+        #'dist_o_1_vh', 
+        #'dist_o_2_vh'
+    }
+)
