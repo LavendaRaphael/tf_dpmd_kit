2f52176a43c3ced70867b7d071142ed7bdeac4d4
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-08-12 14:54:59 +0800

    auto

--- do/do.plm.cv_plt.py
+++ do/do.plm.cv_plt.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def def_plt(
-    dict_label: list,
+    list_label: list,
     str_save: str = None
 ) -> None:
     """
@@ -15,8 +15,8 @@ def def_plt(
     data = np.genfromtxt("COLVAR", dtype=None, names=list_header)
     #print(data.dtype)
     
-    for str_label in dict_label:
-        ax.plot(data['time'], data[str_label], label=str_label)
+    for str_header in list_label:
+        ax.plot(data['time'], data[str_header], label=dict_label[str_header])
     
     ax.legend()
     ax.set_xlabel('Time(ps)')
@@ -25,55 +25,59 @@ def def_plt(
         plt.savefig(str_save, bbox_inches='tight')
     plt.show()
 
+dict_label = {
+    "dist_vp_c": 'R(CP)',
+    "cn_o_1_h": None,
+    'cn_o_2_h': None,
+    'del_cn_o_h': 'cn(O\N{SUBSCRIPT ONE}H)-cn(O\N{SUBSCRIPT TWO}H)',
+    'cn_o_1_vp': None,
+    'cn_o_2_vp': None,
+    'cost_o_1_vp': None, 
+    'cost_o_2_vp': None,
+    'cost_o_1_vh': None, 
+    'cost_o_2_vh': None, 
+    'cost_o_1_h': 'cos dh(O\N{SUBSCRIPT ONE}H)', 
+    'cost_o_2_h': 'cos dh(O\N{SUBSCRIPT TWO}H)', 
+    'restraintbias': None,
+    'up_c_vpbias': None,
+    'up_c_vp.force2': None, 
+    'dist_o_0_h': 'R(O\N{SUBSCRIPT ZERO}H)',
+    'up_o_0_hbias': None, 
+    'up_o_0_h.force2': None, 
+    'cost_o_0_h': 'cos dh(O\N{SUBSCRIPT ZERO}H)', 
+    'dist_vp_o_1': None, 
+    'dist_vp_o_2': None, 
+    'dist_o_1_vh': None, 
+    'dist_o_2_vh': None
+}
+
+
 def_plt(
-    dict_label = [
+    list_label = [
         "dist_vp_c",
-    ]
+    ],
+    str_save = 'cv.dist_vp_c.pdf'
 )
 
 def_plt(
-    dict_label = [
+    list_label = [
         'del_cn_o_h',
-    ]
+    ],
+    str_save = 'cv.del_cn_o_h.pdf'
 )
 def_plt(
-    dict_label = [
+    list_label = [
         'cost_o_1_h', 
         'cost_o_2_h', 
         'cost_o_0_h', 
-    ]
-)
-def_plt(
-    dict_label = [
-        'dist_o_0_h',
-    ]
+    ],
+    str_save = 'cv.cost.pdf'
 )
 '''
 def_plt(
-    dict_label = [
-        #"dist_vp_c",
-        #"cn_o_1_h",
-        #'cn_o_2_h',
-        #'del_cn_o_h',
-        #'cn_o_1_vp',
-        #'cn_o_2_vp',
-        #'cost_o_1_vp', 
-        #'cost_o_2_vp',
-        #'cost_o_1_vh', 
-        #'cost_o_2_vh', 
-        #'cost_o_1_h', 
-        #'cost_o_2_h', 
-        #'restraintbias',
-        #'up_c_vpbias',
-        #'up_c_vp.force2', 
-        #'dist_o_0_h',
-        #'up_o_0_hbias', 
-        #'up_o_0_h.force2', 
-        #'cost_o_0_h', 
-        #'dist_vp_o_1', 
-        #'dist_vp_o_2', 
-        #'dist_o_1_vh', 
-        #'dist_o_2_vh'
-    ]
+    list_label = [
+        'dist_o_0_h',
+    ],
+    str_save = 'cv.dist_o_0_h.pdf'
 )
 '''
