8cdf197ee60fbc191045468e0702554088540f01
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-08-19 23:11:22 +0800

    auto

--- do/do.plm.cv_2d.py
+++ do/do.plm.cv_2d.py
@@ -2,34 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def def_plt(
-    list2d_label: list[list],
+    list2d_cv: list[list],
 ) -> None:
     """
 
     """
-    int_nplot = len(list2d_label)
+    int_nplot = len(list2d_cv)
     fig, axs = plt.subplots(int_nplot, 1, sharex='all')
     if int_nplot==1:
         axs = [axs]
     with open('COLVAR', 'r') as colvar:
         list_header = colvar.readline().split()[2:]
     data = np.genfromtxt("COLVAR", dtype=None, names=list_header)
-    #print(data.dtype)
+    # print(data.dtype)
     for int_i in range(int_nplot):
-        list_label = list2d_label[int_i]
-        for str_header in list_label:
-            axs[int_i].plot(data['time'], data[str_header], label=dict_label[str_header])
-            axs[int_i].legend() 
-    axs[-1].set_xlabel('Time(ps)')
-    axs[0].set_ylabel('CV')
+        axe = axs[int_i]
+        cvx = list2d_cv[int_i][0]
+        cvy = list2d_cv[int_i][1]
+        axe.scatter(
+            x = data[cvx],
+            y = data[cvy]
+        )
+        axe.set_xlabel( dict_label[cvx] )
+        axe.set_ylabel( dict_label[cvy] )
+        str_save = list2d_cv[int_i][2]
+        if str_save:
+            fig.savefig(str_save, bbox_inches='tight')
 
 dict_label = {
     'dist_vp_o_1': None,
     'dist_vp_o_2': None,
     'dist_vp_c': None,
 
-    'distx_vp_o_1': None,
-    'distx_vp_o_2': None,
+    'distx_vp_o_1': 'R\N{Asterisk}(O\N{SUBSCRIPT ONE}V\N{Latin Subscript Small Letter P})',
+    'distx_vp_o_2': 'R\N{Asterisk}(O\N{SUBSCRIPT TWO}V\N{Latin Subscript Small Letter P})',
 
     'cn_o_1_h': None,
     'cn_o_2_h': None,
@@ -52,24 +58,12 @@ for str_key in dict_label.keys():
         dict_label[str_key] = str_key
 #'''
 def_plt(
-    list2d_label = [
-        ['distx_vp_o_1','distx_vp_o_2'],
-        ['cost_o_h'],
-        ['pathspath'],
-        ['pathzpath'],
-        ['dist_o_0_h','cost_o_0_h'],
-        ['dist_vp_o_1','dist_vp_o_2'],
+    list2d_cv = [
+        #['distx_vp_o_1','distx_vp_o_2','distx.pdf'],
+        #['distx_vp_o_1','cost_o_h','distx1_cost.pdf'],
+        ['distx_vp_o_2','cost_o_h','distx2_cost.pdf'],
+        #['pathspath','pathzpath','path.pdf'],
     ]
 )
-'''
-def_plt(
-    list2d_label = [
-        ['dist_vp_c'],
-        ['cn_o_1_h','cn_o_2_h','del_cn_o_h'],
-        ['cost_o_1_h','cost_o_2_h'],
-        ['pathspath','pathzpath'],
-        ['dist_o_0_h','cost_o_0_h'],
-    ]
-)
-'''
+#'''
 plt.show()
