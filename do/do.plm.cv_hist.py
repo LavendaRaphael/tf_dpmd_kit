d9ff5aaeb000afafed48ae2f6737f753daa05ed5
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-09-04 16:33:40 +0800

    auto

--- do/do.plm.cv_hist.py
+++ do/do.plm.cv_hist.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def def_plt(
-    list2d_label: list[list],
+    list2d_header: list[list],
     str_save = None
 ) -> None:
     """
 
     """
-    int_nplot = len(list2d_label)
+    int_nplot = len(list2d_header)
     fig, axs = plt.subplots(int_nplot, 1)
     if int_nplot==1:
         axs = [axs]
@@ -19,8 +19,8 @@ def def_plt(
     #print(data.dtype)
 
     for int_i in range(int_nplot):
-        str_header = list2d_label[int_i][0]
-        tup_range = list2d_label[int_i][1]
+        str_header = list2d_header[int_i][0]
+        tup_range = list2d_header[int_i][1]
         axs[int_i].hist(
             data[str_header], 
             label = dict_label[str_header],
@@ -40,31 +40,19 @@ dict_label = {
     'del_dist_vp_o': '\N{Greek Capital Letter Delta}R(OV\N{Latin Subscript Small Letter P})',
     'dist_vp_c': 'R(CV\N{Latin Subscript Small Letter P})',
 
-    'cnx_vp_o_1': None,
-    'cnx_vp_o_2': None,
-
-    'cn_o_1_h': None,
-    'cn_o_2_h': None,
     'del_cn_o_h': '\N{Greek Capital Letter Delta}cn(OH)',
 
-    'cost_o_1_vh': None, 
-    'cost_o_2_vh': None,
     'cost_o_h': 'cos dh(OH)',
-    'cost_o_1_h': None,
-    'cost_o_2_h': None,
 
     'dist_o_0_h': 'R(O\N{SUBSCRIPT ZERO}H\N{SUBSCRIPT ZERO})',
     'cost_o_0_h': 'cos dh(O\N{SUBSCRIPT ZERO}H)', 
 
     'cnn_o_w_hmin': r'cn(O$_W$H)$_{min}$'
 }
-for str_key in dict_label.keys():
-    if not dict_label[str_key]:
-        dict_label[str_key] = str_key
 
 #'''
 def_plt(
-    list2d_label = [
+    list2d_header = [
         ['dist_o_0_h', (0.9, 1.3)],
         ['cost_o_0_h', (-1, -0.9)],
         ['cnn_o_w_hmin', (1.8,1.9)]
@@ -74,7 +62,7 @@ def_plt(
 #'''
 '''
 def_plt(
-    list2d_label = [
+    list2d_header = [
         ['dist_vp_c', (1.2,1.5)],
         ['del_dist_vp_o', (2.1,2.4)],
         ['cost_o_h', (0.8,1)],
