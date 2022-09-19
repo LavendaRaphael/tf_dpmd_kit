2b7adb601ad5e3e2cfb120368b634f52fbef8e10
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-09-19 09:52:51 +0800

    auto

--- do/do.model_devi.devi_analysis.py
+++ do/do.model_devi.devi_analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 array_devi = np.genfromtxt("model_devi.out", names=True, dtype=None)
-tuple_trust = (0.15,0.5)
+tuple_trust = (0.15,0.25)
 array_candidate_index = np.where( (array_devi["max_devi_f"]>tuple_trust[0]) & (array_devi["max_devi_f"]<tuple_trust[1]) )[0]
 array_failed_index = np.where( array_devi["max_devi_f"]>tuple_trust[1] )[0]
 
