9414a265d66a60200e51885b0a896a80c33c27ca
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-09-28 14:22:43 +0800

    auto

--- do/do.qe.evp_plt.py
+++ do/do.qe.evp_plt.py
@@ -1,27 +1,33 @@
-import numpy
 from matplotlib import pyplot
 import numpy as np
 import matplotlib
 
-matplotlib.rcParams['font.size']=15
-matplotlib.rcParams['font.family']='sans-serif'
-matplotlib.rcParams['font.sans-serif']=["Arial"]
+def def_time_ekin_plt(
+    str_evp:  str='cp/cp.evp',
+    str_save: str='time_ekin.pdf'
+) -> None:
 
-str_evp = 'cp/cp.evp'
-with open(str_evp, 'r') as open_evp:
-    list_header = open_evp.readline().split()[1:-2]
-np_evp = np.genfromtxt(str_evp, names=list_header)
-print(np_evp,np_evp.dtype)
+    matplotlib.rcParams['font.size']=15
+    matplotlib.rcParams['font.family']='sans-serif'
+    matplotlib.rcParams['font.sans-serif']=["Arial"]
+    
+    with open(str_evp, 'r') as open_evp:
+        list_header = open_evp.readline().split()[1:-2]
+    np_evp = np.genfromtxt(str_evp, names=list_header)
+    
+    fig, ax = pyplot.subplots()
+    ax.plot( np_evp['timeps'], np_evp['ekinc'], label = 'Ekin_e', linewidth=2)
+    ax.plot( np_evp['timeps'], (np_evp['econs']-np_evp['etot'])/10, label = 'Ekin_ion/10', linewidth=2)
+    
+    ax.legend()
+    ax.set_xlabel('Time (ps)')
+    ax.set_ylabel(f'Ekin ($E_h$)')
+    ax.set_ylim(0, 0.1)
+    fig.set_size_inches(8, 4)
+    if str_save:
+        fig.savefig(str_save, bbox_inches='tight')
 
-fig, ax = pyplot.subplots()
-ax.plot( np_evp['timeps'], np_evp['ekinc'], label = 'Ekin_e', linewidth=2)
-ax.plot( np_evp['timeps'], (np_evp['econs']-np_evp['etot'])/10, label = 'Ekin_ion/10', linewidth=2)
+def_time_ekin_plt()
 
-ax.legend()
-ax.set_xlabel('Time (ps)')
-ax.set_ylabel(f'Ekin ($E_h$)')
-ax.set_ylim(0, 0.1)
-fig.set_size_inches(8, 4)
-fig.savefig('time_ekin.pdf', bbox_inches='tight')
 pyplot.show()
 
