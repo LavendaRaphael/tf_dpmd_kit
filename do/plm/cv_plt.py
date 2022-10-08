e1ea8371794417e119671173d9d4ba047bcf81f3
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-10-08 20:01:39 +0800

    auto

--- do/plm/cv_plt.py
+++ do/plm/cv_plt.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def def_plt(
-    list2d_header: list[list],
+    list_header: list,
     float_timescale: float
 ) -> None:
 
@@ -29,11 +29,11 @@ def def_plt(
     axs[0].set_ylabel('CV')
 
 def def_multi_plt(
-    list2d_header: list[list],
+    list_header: list,
     float_timescale: float
 ) -> None:
 
-    int_nplot = len(list2d_header)
+    int_nplot = len(list_header)
     fig, axs = plt.subplots(int_nplot, 1, sharex='all')
     if int_nplot==1:
         axs = [axs]
@@ -52,24 +52,23 @@ def def_multi_plt(
                 else:
                     break
             data = np.genfromtxt(list_tmp, dtype=None, names=list_field, invalid_raise=False)
-            int_count += 1
             for int_i in range(int_nplot):
-                list_header = list2d_header[int_i]
-                for str_header in list_header:
-                    if str_header not in data.dtype.names:
-                        continue
-                    if str_header not in dict_label:
-                        str_label = str_header
-                    else:
-                        str_label = dict_label[str_header]
-                    if int_count != 1:
-                        str_label = None
-                    axs[int_i].scatter(data['time']*float_timescale, data[str_header], label=str_label, s=0.5)
-                    axs[int_i].legend(loc='upper left')
+                str_header = list_header[int_i]
+                if str_header not in data.dtype.names:
+                    continue
+                axs[int_i].scatter(data['time']*float_timescale, data[str_header], s=0.5)
             if str_line[0] != '#':
                 break
             list_tmp = [str_line]
             list_field = str_line.split()[2:]
+    for int_i in range(int_nplot):
+        str_header = list_header[int_i]
+        if str_header not in dict_label:
+            str_label = str_header
+        else:
+            str_label = dict_label[str_header]
+        axs[int_i].set_ylabel(str_label)
+
 dict_label = {
     'dist_vp_o_1': 'R(O\N{SUBSCRIPT ONE}V\N{Latin Subscript Small Letter P})',
     'dist_vp_o_2': 'R(O\N{SUBSCRIPT TWO}V\N{Latin Subscript Small Letter P})',
@@ -80,15 +79,15 @@ dict_label = {
 }
 
 def_multi_plt(
-    list2d_header = [
-        ['dist_vp_c'],
-        ['dh2x_o_0_h'],
-        ['metadbias'],
-        ['dist_o_0_h'],
-        #['uw_dist_o_0_hbias'],
-        ['cn_o_0_h'],
-        #['uw_cn_o_0_hbias'],
-        ['cnn_o_w_hmin']
+    list_header = [
+        'dist_vp_c',
+        'dh2x_o_0_h',
+        'metadbias',
+        'dist_o_0_h',
+        #'uw_dist_o_0_hbias',
+        'cn_o_0_h',
+        #'uw_cn_o_0_hbias',
+        'cnn_o_w_hmin'
     ],
     float_timescale = 1/0.0005/40
 )
