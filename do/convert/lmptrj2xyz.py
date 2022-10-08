d791ad676d3da9f5132233f3e81ba2d4f43db36e
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-10-08 22:07:21 +0800

    auto

--- do/convert/lmptrj2xyz.py
+++ do/convert/lmptrj2xyz.py
@@ -1,9 +1,11 @@
-import dpdata
 import ase.io
-import numpy
 
-dp_sys = dpdata.System('traj.lammpstrj', fmt='lammps/dump', type_map=['O','H','C'])
-print(dp_sys)
-ase_atoms = dp_sys.to('ase/structure')
-list_cell = ase_atoms[0].get_cell().cellpar()
-ase.io.write( filename='traj.xyz', images=ase_atoms, format='extxyz', comment=f' {list_cell[0]} {list_cell[1]} {list_cell[2]}' )
+list_ase = ase.io.read(
+    filename = 'traj.lammpstrj',
+    format = 'lammps-dump-text',
+    index = ':',
+    specorder = ['O','H','C']
+)
+print(list_ase[0])
+
+ase.io.write( filename='traj.xyz', images=list_ase, format='xyz' )
