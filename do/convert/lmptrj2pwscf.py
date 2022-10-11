5f92bd16b54d0163ac3534ad884325df9e66f9e7
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-10-11 10:05:15 +0800

    auto

--- do/convert/lmptrj2pwscf.py
+++ do/convert/lmptrj2pwscf.py
@@ -1,5 +1,5 @@
 import ase.io
-import numpy
+import numpy as np
 from tf_dpmd_kit import convert
 
 list_ase = ase.io.read(
@@ -10,7 +10,10 @@ list_ase = ase.io.read(
 )
 print(list_ase[0])
 
-np_snap = [352800]
+#np_snap = range(466300, 467200, 10)
+#np_snap = range(481100, 489000, 100)
+#np_snap = range(361400, 362900, 10)
+#np_snap = range(177200, 177900, 10)
 
 convert.list_ase2pwscf(
     list_ase = list_ase,
