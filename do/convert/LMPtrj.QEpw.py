cc1509d1ddf113165d94daf1cac05bb4f0aa1b28
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-10-16 14:16:22 +0800

    auto

--- do/convert/LMPtrj.QEpw.py
+++ do/convert/LMPtrj.QEpw.py
@@ -10,10 +10,7 @@ list_ase = ase.io.read(
 )
 print(list_ase[0])
 
-#np_snap = range(466300, 467200, 10)
-#np_snap = range(481100, 489000, 100)
-#np_snap = range(361400, 362900, 10)
-#np_snap = range(177200, 177900, 10)
+np_snap = range(68000, 72400, 50)
 
 convert.list_ase2pwscf(
     list_ase = list_ase,
