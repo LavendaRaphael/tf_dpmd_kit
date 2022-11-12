c22dad7469785ceedf671c4b0a27a7f96c137e44
LavendaRaphael <feifei.tian.cn@gmail.com>
2022-11-12 22:34:43 +0800

    auto

--- do/plm/driver_colvar.sh
+++ do/plm/driver_colvar.sh
@@ -1,3 +1,3 @@
 source ~/.config/.tianff
 source ${homedir}/.local/bin/bashrc_plm.sh
-plumed driver --plumed plm_driver.in --noatoms --timestep 0.0005 --trajectory-stride 10
+plumed driver --plumed plm.in --noatoms --timestep 0.0005 --trajectory-stride 10
